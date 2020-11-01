e575e89c763da6bfbf28d35e0b65b8e0b1c28198
dhruvchawla102 <dhruvchawla102@gmail.com>
2020-11-01 16:14:35 +0530

    Updates

---
+++ precache-manifest.7bb28257d424a44abbdf200d51b0aab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00278a51b70f6480d538d6b912a30270",
+    "url": "/college_1/index.html"
+  },
+  {
+    "revision": "7a8f0295cc745908b361",
+    "url": "/college_1/static/css/main.c78346db.chunk.css"
+  },
+  {
+    "revision": "53f96bb38b314d3fca89",
+    "url": "/college_1/static/js/2.27369396.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/college_1/static/js/2.27369396.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a8f0295cc745908b361",
+    "url": "/college_1/static/js/main.8495adcb.chunk.js"
+  },
+  {
+    "revision": "777ce285d3999412d137",
+    "url": "/college_1/static/js/runtime-main.3f686e4c.js"
+  }
+]);
